fe356409913feca62458a9076b2a893ae71f594f
s.shivasurya <s.shivasurya@gmail.com>
2020-10-24 11:15:05 -0400

    added empty if block for testcase

--- app/src/main/java/app/sample/security/MainActivity.java
+++ app/src/main/java/app/sample/security/MainActivity.java
@@ -1,3 +1,4 @@
+
 package app.sample.security;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int data = 0;
+
+        if(data == 0){
+            if(data == 1){
+
+            }
+        }
     }
 }
